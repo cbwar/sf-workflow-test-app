81087f17d1ae0c0600fc314979b40b4a86246d42
Raphael Lisch <cbwar@protonmail.com>
2022-01-29 01:18:29 +0100

    feat: add initial Document entity

---
+++ src/Entity/Document.php
@@ -0,0 +1,80 @@
+<?php /** @noinspection PhpPropertyOnlyWrittenInspection */
+
+namespace App\Entity;
+
+use App\Repository\DocumentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: DocumentRepository::class)]
+class Document
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $path;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $status;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private string $description;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
